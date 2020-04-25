33f3dccf9788e7619c8c3bd0f7683ebd1d00b292
ashok <ashok.g@videoken.com>
2020-04-25 12:51:02 -0500

    Updates

---
+++ precache-manifest.7f2592109a6994e708fafacfa7f9b25b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bc60953b04e40b909076177d6d9ae6",
+    "url": "/ashokgurusamy/my-chat.io/index.html"
+  },
+  {
+    "revision": "3db821f7897376aff5ae",
+    "url": "/ashokgurusamy/my-chat.io/static/css/2.4cb31e96.chunk.css"
+  },
+  {
+    "revision": "ef1d6c5737e57e78e9d6",
+    "url": "/ashokgurusamy/my-chat.io/static/css/main.5186ee6a.chunk.css"
+  },
+  {
+    "revision": "3db821f7897376aff5ae",
+    "url": "/ashokgurusamy/my-chat.io/static/js/2.e27f4d3a.chunk.js"
+  },
+  {
+    "revision": "ef1d6c5737e57e78e9d6",
+    "url": "/ashokgurusamy/my-chat.io/static/js/main.6253af8c.chunk.js"
+  },
+  {
+    "revision": "c78f9dc02806bff320b5",
+    "url": "/ashokgurusamy/my-chat.io/static/js/runtime~main.8694be02.js"
+  },
+  {
+    "revision": "7443c012820c810cffc27caaefe9e8fe",
+    "url": "/ashokgurusamy/my-chat.io/static/media/paper-plane (1).7443c012.svg"
+  },
+  {
+    "revision": "8ee1e567f83d35c955c62afa43fc52f2",
+    "url": "/ashokgurusamy/my-chat.io/static/media/profile.8ee1e567.jpg"
+  }
+]);
